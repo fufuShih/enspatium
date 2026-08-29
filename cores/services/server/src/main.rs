d78b7daf6dcf6aded40786f63b676b448f212275
Felix Shih <zx8714245@gmail.com>
2026-08-29 15:27:38 +0800

    feat: extract the http to crates

--- cores/services/server/src/main.rs
+++ cores/services/server/src/main.rs
@@ -1,12 +1,9 @@
 use std::env;
 
-use axum::{Router, routing::get};
-use tower_http::trace::TraceLayer;
 use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() {
-    
     tracing_subscriber::fmt()
         .with_env_filter(
             EnvFilter::try_from_default_env()
@@ -16,9 +13,7 @@ async fn main() {
 
     let address = env::var("APP_ADDR").unwrap_or_else(|_| "127.0.0.1:3000".to_owned());
 
-    let app = Router::new()
-        .route("/health", get(health))
-        .layer(TraceLayer::new_for_http());
+    let app = enspatium_http::router();
 
     let listener = tokio::net::TcpListener::bind(&address)
         .await
@@ -27,8 +22,4 @@ async fn main() {
     tracing::info!(%address, "server started");
 
     axum::serve(listener, app).await.expect("server failed");
-}
-
-async fn health() -> &'static str {
-    "ok"
 }
