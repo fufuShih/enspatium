401bd4e0d406fd09d3ba48c168a2eba7ec464473
Felix Shih <zx8714245@gmail.com>
2026-09-04 01:33:56 +0800

    feat: init the app

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { router } from './router'
+import { RouterProvider } from 'react-router/dom'
+import { queryClient } from './queryClient'
+
+
+const App = () => {
+  return (
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </StrictMode>
+  )
+}
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />,
 )
